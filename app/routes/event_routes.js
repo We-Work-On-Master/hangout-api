7540e2a1cab18d37fc8e76408c8cb824335da685
Kevinoshea <kevinbarretoshea@gmail.com>
2020-03-12 14:21:11 -0400

    Reordered event routes in order to work not/signed
    Order for use in signed in vs not signed in

--- app/routes/event_routes.js
+++ app/routes/event_routes.js
@@ -27,22 +27,6 @@ const requireToken = passport.authenticate('bearer', { session: false })
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
-// INDEX SIGNED IN
-// GET /events
-router.get('/events', requireToken, (req, res, next) => {
-  Event.find()
-    .then(events => {
-      // `events` will be an array of Mongoose documents
-      // we want to convert each one to a POJO, so we use `.map` to
-      // apply `.toObject` to each one
-      return events.map(event => event.toObject())
-    })
-    // respond with status 200 and JSON of the events
-    .then(events => res.status(200).json({ events: events }))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
-
 // INDEX SIGNED OUT
 // GET /events/
 router.get('/events/openall', (req, res, next) => {
@@ -67,6 +51,24 @@ router.get('/events/:id', requireToken, (req, res, next) => {
     .then(handle404)
     // if `findById` is succesful, respond with 200 and "event" JSON
     .then(event => res.status(200).json({ event: event.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+
+
+// INDEX SIGNED IN
+// GET /events
+router.get('/events', requireToken, (req, res, next) => {
+  Event.find()
+    .then(events => {
+      // `events` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return events.map(event => event.toObject())
+    })
+    // respond with status 200 and JSON of the events
+    .then(events => res.status(200).json({ events: events }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
